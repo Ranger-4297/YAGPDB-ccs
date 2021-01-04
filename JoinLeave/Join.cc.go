fac6e4da87e51789ee7b66505faab82147c54b05
Dynamic <72880161+DiscDynamic@users.noreply.github.com>
2021-01-04 15:13:18 +0000

    added public join log
    As well as logging userjoins in the a staff log
    You now get a join message sent to a channe, e.x #welcome

--- JoinLeave/Join.cc.go
+++ JoinLeave/Join.cc.go
@@ -1,21 +1,35 @@
 {{/*
         Made by Rhyker/Ranger (779096217853886504)
 
-    Trigger Type: `Join Message in channrl`
+    Trigger Type: `Join Message in channel`
 ©️ Dynamic 2021
 MIT License
 */}}
 
 
+{{/* Configuration values start */}}
+{{$welcomeChannel := 786752542171594752}}
+{{/* Configuration values end */}}
+
+{{/* Only edit below if you know what you're doing except for the 21st line. Add your ban appeal link at the end. (: rawr */}}
+
 {{if .UsernameHasInvite}}
-{{ $silent := (execAdmin "ban" .User.ID "ad blocked") }}
+{{$silent := (execAdmin "ban" .User.ID "ad blocked")}}
 {{else}}
-{{$embed := cembed
+{{$logEmbed := cembed
             "author" (sdict "url" (.User.AvatarURL "4096") "name" "User Joined" "icon_url" (.User.AvatarURL "1024"))
             "description" (print  .User.Mention "\n[" .User.String "]" " : " "`" .User.ID "`" "\nAccount created " "**" currentUserAgeHuman "** ago" "\nWe now have" "** " .Guild.MemberCount " **" "members")
             "footer" (sdict "text" " ")
             "timestamp" currentTime
             "color" 3247335
             }}
-{{sendMessage nil $embed}}
+{{sendMessage nil $logEmbed}}
+{{$welcomeEmbed := cembed
+            "author" "name" "User Joined!" "icon_url" (.User.AvatarURL "2024")
+            "description" (print "Hello " .User.String "! Welcome to " .Guild.Name "! Check the DM I sent you!\nWe now have `" .Guild.MemberCount "` members!")
+            "footer" (sdict "text" " ")
+            "timestamp" currentTime
+            "color" 65419
+            }}
+{{sendMessage $welcomeChannel $welcomeEmbed}}
 {{end}}
