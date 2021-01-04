00b1dc8155bfe7e2e579a5dfde44e39f12513104
Dynamic <72880161+DiscDynamic@users.noreply.github.com>
2021-01-04 15:18:14 +0000

    Colored the Mute log to default

--- Moderation/Mute.cc.go
+++ Moderation/Mute.cc.go
@@ -2,7 +2,7 @@
         Made by Rhyker (779096217853886504)
 
     Trigger Type: `Mute DM`
-©️ Dynamic 2021
+:copyright: Dynamic 2021
 MIT License
 */}}
 
@@ -40,6 +40,6 @@ MIT License
             "description" (print ":mute: **Muted:** *" $user.Username "#" $user.Discriminator "* `(ID " $user.ID ")`\n:receipt: **Channel:** <#" .Channel.ID ">\n:page_facing_up: **Reason:** " .Reason "\n:clock12: **Time:** " ( joinStr " " (( currentTime.Add 0).Format "15:04 GMT")))
             "thumbnail" (sdict "url" ($user.AvatarURL "256"))
             "footer" (sdict "text" (print "Duration: " .HumanDuration ))
-            "color" 7372944
+            "color" 5731006
             }}
 {{sendMessage $LogChannel $Log}}
