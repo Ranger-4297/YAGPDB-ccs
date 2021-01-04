99c1f2f896fb67a346f082fc7c371dcdadddd212
Dynamic <72880161+DiscDynamic@users.noreply.github.com>
2021-01-04 14:06:42 +0000

    Added credits + DISBOARD UserID ty Ww

--- DISBOARD/!dpage.cc.go
+++ DISBOARD/!dpage.cc.go
@@ -1,8 +1,9 @@
 {{/*
         Made by Rhyker (779096217853886504)
-
+        Modified by WickedWizard#3588
+        
     Trigger Type: `Exact`
-    Trigger: `\A!d\sbump(?:\s+|\z)`
+    Trigger: `!d page`
 ©️ Dynamic 2021
 MIT License
 */}}
@@ -10,7 +11,7 @@ MIT License
 
 {{/* Only edit below if you know what you're doing (: rawr */}}
 
-{{execAdmin "clean" 1}}
+{{execAdmin "clean" 1 302050872383242240}}
 {{sendMessage nil (cembed
             "title" "DISBOARD: The public server list"
                 "url" (print "https://disboard.org/server/" .Guild.ID)
