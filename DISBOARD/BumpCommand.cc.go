737f16c04f690fd372e039519f5b6215146aba1f
soudblox <73058244+soudblox@users.noreply.github.com>
2021-01-05 21:57:42 +0700

    make ping on ~~ after someone bump

--- DISBOARD/BumpCommand.cc.go
+++ DISBOARD/BumpCommand.cc.go
@@ -12,7 +12,7 @@ MIT License
 
 {{/* Configuration values start */}}
 {{$bumpNotifChannel := 787957757022175232}} {{/* channel to send the message notifying users it's possible to bump again */}}
-{{$bumpPing := "<@&784132355379036194>"}} {{/* role to ping when it's possible to bump once again */}}
+{{$bumpPing := "784132355379036194"}} {{/* role to ping when it's possible to bump once again */}}
 {{$bumpChannel := 787957898872487956}} {{/* Channel to mention in the notification for users to have a quick portal */}}
 {{$bumpLog := 794365711614345267}} {{/* Channel to log bumps */}}
 {{/* Configuration values end */}}
@@ -23,8 +23,8 @@ MIT License
 {{end}}
 
 {{if .ExecData}}
-    {{sendMessageNoEscape $bumpNotifChannel (complexMessage 
-            "content" $bumpPing
+{{$id := sendMessageNoEscapeRetID $bumpNotifChannel (complexMessage 
+            "content" (print "<@&" $bumpPing ">")
             "embed" (cembed
             "title" "DISBOARD: The public server list"
                 "url" (print "https://disboard.org/server/" .Guild.ID)
@@ -44,6 +44,15 @@ MIT License
             "image" (sdict "url" "https://disboard.org/images/bot-command-image-bump.png")
             "color" 4436910
             )}}
+            {{editMessageNoEscape $bumpNotifChannel $id (complexMessageEdit
+            "content" (print "~~<@&" $bumpPing ">~~")
+            "embed" (cembed
+            "title" "DISBOARD: The public server list"
+                "url" (print "https://disboard.org/server/" .Guild.ID)
+            "description" (print "~~You can now bump us in <#" $bumpChannel "> using `!d bump`!~~\nWhy not leave us a review __[here](https://disboard.org/dashboard/reviews)__!")
+            "thumbnail" (sdict "url" "https://disboard.org/images/bot-command-image-thumbnail.png")
+            "color" 4436910
+            ))}}
         {{sendMessage $bumpLog (cembed
             "title" "DISBOARD: The public server list"
                 "url" (print "https://disboard.org/server/" .Guild.ID)
