a0a77e75e9ffd2a3c21939635389e081c7a33c73
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-02-13 17:11:55 +0000

    (fix)Update

--- JoinLeave/Leave.cc.go
+++ JoinLeave/Leave.cc.go
@@ -1,17 +1,31 @@
 {{/*
-        Made by Rhyker/Ranger (779096217853886504)
+        Made by Ranger (779096217853886504)
 
-    Trigger Type: `Join Message in channel`
+    Trigger Type: `Leave Message in channel`
 ©️ Dynamic 2021
 MIT License
 */}}
 
 
-{{$embed := cembed
+{{/* Configuration values start */}}
+{{$Log:= 784132358085017602}} {{/* Join log channel */}}
+{{/* Configuration values end */}}
+
+{{/* Only edit below if you know what you're doing except for the 21st line. Add your ban appeal link at the end. (: rawr */}}
+
+{{$logEmbed := cembed
             "author" (sdict "url" (.User.AvatarURL "4096") "name" "User Left" "icon_url" (.User.AvatarURL "1024"))
-            "description" (print  .User.Username "\n[" .User.String "]" " : " "`" .User.ID "`" "\nAccount created " "**" currentUserAgeHuman "** ago" "\nWe now have" "** " .Guild.MemberCount " **" "members")
+            "description" (print  .User.Mention "\n[" .User.String "]" " : " "`" .User.ID "`" "\nAccount created " "**" currentUserAgeHuman "** ago" "\nWe now have" "** " .Guild.MemberCount " **" "members")
             "footer" (sdict "text" " ")
             "timestamp" currentTime
             "color" 16711680
             }}
-{{sendMessage nil $embed}}
+{{sendMessage $Log $logEmbed}}
+{{$leaveEmbed := cembed
+            "author" (sdict "url" (.User.AvatarURL "4096") "name" "User left!" "icon_url" (.User.AvatarURL "1024"))
+            "description" (print "Welp " .User.String " Left " .Guild.Name "~~ :( Guess they were cool enough~~\nWe now have `" .Guild.MemberCount "` members!")
+            "footer" (sdict "text" " ")
+            "timestamp" currentTime
+            "color" 16711680
+            }}
+{{sendMessage nil $leaveEmbed}}
