a7b616a7887946b30c7eb976dd74da2089b702ba
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-02-13 14:50:59 +0000

    Update Apply.cc.go

--- Forms/Apply.cc.go
+++ Forms/Apply.cc.go
@@ -1,5 +1,5 @@
 {{/*
-        Made by Rhyker (779096217853886504)
+        Made by Ranger (779096217853886504)
 
     Trigger Type: `Command`
     Trigger: `Apply`
@@ -9,16 +9,18 @@ MIT License
 
 
 {{/* Configuration values start */}}
+{{$FormLink := "https://realsite.co.uk/application"}} {{/* Your servers application link */}}
 {{$logchannel := 794365711614345267}}
+{{$IconType := "gif"}} {{/* set to `png` if your server uses a static icon */}}
 {{/* Configuration values end */}}
 
 {{/* Only edit below if you know what you're doing except for the 21st line. Add your staff application link at the end. (: rawr */}}
 
-Check your DM's for info!
+{{sendMessage nil "Check your DM's for info!"}}
 {{$embed := cembed
             "author" (sdict "url" (print "https://discord.com/channels/" .Guild.ID) "name" (print .Guild.Name " staff applications") "icon_url" (.User.AvatarURL "1024"))
             "description" (print  .User.Mention ",\nHello " .User.Username "\nThank you for taking interest in our servers staff team!\nPlease keep in mind that asking about the status of your application may warrant it's status as `denied`.\n[Application form](application)")
-            "thumbnail" (sdict "url" (print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon ".gif"))
+            "thumbnail" (sdict "url" (print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon "." $IconType ))
             "footer" (sdict "text" " ")
             "timestamp" currentTime
             "color" 4645612
@@ -29,7 +31,7 @@ Check your DM's for info!
 {{$logembed := cembed
             "author" (sdict "icon_url" (.User.AvatarURL "1024") "name" (print .User.String " (ID " .User.ID ")"))
             "description" (print "**✉️ Application command notification**\n" .User.Mention " Has requested a staff application.")
-            "thumbnail" (sdict "url" (print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon ".gif"))
+            "thumbnail" (sdict "url" (print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon "." $IconType))
             "footer" (sdict "text" " ")
             "timestamp" currentTime
             "color" 4645612
