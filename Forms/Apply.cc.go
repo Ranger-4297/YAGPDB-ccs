ec6bfc45e45c52917f79ba1256707c3d9cbe9362
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-02-15 16:00:27 +0000

    (fix)Guild icon support

--- Forms/Apply.cc.go
+++ Forms/Apply.cc.go
@@ -16,11 +16,19 @@ MIT License
 
 {{/* Only edit below if you know what you're doing except for the 21st line. Add your staff application link at the end. (: rawr */}}
 
+{{$icon := ""}}
+{{$name := printf "%s (%d)" .Guild.Name .Guild.ID}}
+{{if .Guild.Icon}}
+    {{$ext := "webp"}}
+    {{if eq (slice .Guild.Icon 0 2) "a_"}} {{$ext = "gif"}} {{end}}
+    {{$icon = printf "https://cdn.discordapp.com/icons/%d/%s.%s" .Guild.ID .Guild.Icon $ext}}
+{{end}}
+
 {{sendMessage nil "Check your DM's for info!"}}
 {{$embed := cembed
             "author" (sdict "url" (print "https://discord.com/channels/" .Guild.ID) "name" (print .Guild.Name " staff applications") "icon_url" (.User.AvatarURL "1024"))
             "description" (print  .User.Mention ",\nHello " .User.Username "\nThank you for taking interest in our servers staff team!\nPlease keep in mind that asking about the status of your application may warrant it's status as `denied`.\n[Application form](application)")
-            "thumbnail" (sdict "url" (print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon "." $IconType ))
+            "thumbnail" (sdict "url" $icon)
             "footer" (sdict "text" " ")
             "timestamp" currentTime
             "color" 4645612
