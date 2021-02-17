1b0f96e833c7053b1139ffa56ac39b87d4510824
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-02-17 05:08:08 +0000

    (create)Revive

---
+++ Misc/Revive.cc.go
@@ -0,0 +1,29 @@
+{{/*
+        Made by Ranger (779096217853886504)
+
+    Trigger Type: `Command`
+    Trigger: `Revive`
+©️ Dynamic 2021
+MIT License
+*/}}
+
+{{/* Configuration values start */}}
+{{$activityPing := 799160232823226368}} {{/* RoleID of the role to ping when reviving chat */}}
+{{$Cooldown := 3600}} {{/* Cooldown in SECONDS */}}
+{{/* Configuration values end */}}
+
+{{if $cooldown := dbGet 0 "cooldown" }}
+    {{sendMessage nil (cembed
+            "author" (sdict "icon_url" (.User.AvatarURL "1024") "name" (print "Sorry, you're on cooldown"))
+            "description" (print ":warning: This command is still on cooldown for: " (humanizeDurationSeconds ($cooldown.ExpiresAt.Sub currentTime)) )
+            "color" 14043208
+            )}}
+    {{else}}
+	{{dbSetExpire 0 "cooldown" "cooldown" $Cooldown}}
+	{{sendMessageNoEscape nil (complexMessage
+            "content" (print "<@&" $activityPing ">")
+			      "embed" (cembed
+			      "description"  (print .User.Mention " summons you with an activity ping!")
+			      "color" 4436910
+			      ))}}
+{{end}}
