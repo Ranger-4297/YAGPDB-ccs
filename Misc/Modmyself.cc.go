2c835d30d4cf126d47c7afbd695f16a2a766ca6e
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-02-17 05:10:35 +0000

    (fix)Typo and indentation

--- Misc/Modmyself.cc.go
+++ Misc/Modmyself.cc.go
@@ -1,27 +1,27 @@
 {{/*
-		Made by Ranger (779096217853886504)
+        Made by Ranger (779096217853886504)
 
-	Trigger Type: `Command`
-	Trigger: `Modmyself`
+    Trigger Type: `Command`
+    Trigger: `Modmyself`
 ©️ Dynamic 2021
 MIT License
 */}}
 
 {{/* Configuration values start */}}
-{{$serverLink := "https://discord.gg/ekMQH384KC"}} {{/* Your servers invitr link */}}
+{{$serverLink := "https://discord.gg/ekMQH384KC"}} {{/* Your servers invite link */}}
 {{/* Configuration values end */}}
 
 {{/* Only edit below if you know what you're doing except for the 21st line. Add your staff application link at the end. (: rawr */}}
 
 {{$random := randInt 4}}
 {{if le $random 0}}
-	{{execAdmin "warn" .User.ID "you fookin donut, you modded yourself"}}
-	{{else if le $random 1}}
-		{{execAdmin "mute" .User.ID (randInt 61) "you fookin donut, you modded yourself"}}
-		{{else if le $random 2}}
-			{{execAdmin "kick" .User.ID (print "you fookin donut, you modded yourself :/ rejoin at [Dynamic](" $serverLink ")")}}
-			{{else if le $random 3}}
-				{{execAdmin "ban" .User.ID (print "you fookin donut, you modded yourself\n:/rejoin in an hour (Yes. You have to wait) at [" .Server.Name "](" $serverLink ")") "-d" (print (randInt 61) "mins")}}
-		{{end}}
-	{{end}}
+    {{execAdmin "warn" .User.ID "you fookin donut, you modded yourself"}}
+    {{else if le $random 1}}
+        {{execAdmin "mute" .User.ID (randInt 61) "you fookin donut, you modded yourself"}}
+        {{else if le $random 2}}
+            {{execAdmin "kick" .User.ID (print "you fookin donut, you modded yourself :/ rejoin at [Dynamic](" $serverLink ")")}}
+            {{else if le $random 3}}
+                {{execAdmin "ban" .User.ID (print "you fookin donut, you modded yourself\n:/rejoin in an hour (Yes. You have to wait) at [" .Server.Name "](" $serverLink ")") "-d" (print (randInt 61) "mins")}}
+        {{end}}
+    {{end}}
 {{end}}
