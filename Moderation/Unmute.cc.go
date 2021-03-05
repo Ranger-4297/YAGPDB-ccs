2ac53eb0a69abcfd6cf327547047b819214b212b
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-03-05 22:57:38 +0000

    (fix)removed user variable

--- Moderation/Unmute.cc.go
+++ Moderation/Unmute.cc.go
@@ -1,7 +1,7 @@
 {{/*
         Made by Ranger (779096217853886504)
 
-    Trigger Type: `Unmute DM`
+    Trigger Type: `Warn DM`
 ©️ Dynamic 2021
 MIT License
 */}}
@@ -13,36 +13,23 @@ MIT License
 
 {{/* Only edit below if you know what you're doing (: rawr */}}
 
-{{$icon := ""}}
-{{$name := printf "%s (%d)" .Guild.Name .Guild.ID}}
-{{if .Guild.Icon}}
-	{{$ext := "webp"}}
-	{{if eq (slice .Guild.Icon 0 2) "a_"}} {{$ext = "gif"}} {{end}}
-	{{$icon = printf "https://cdn.discordapp.com/icons/%d/%s.%s" .Guild.ID .Guild.Icon $ext}}
-{{end}}
-
 {{if gt ( toInt ( currentTime.UTC.Format "15" ) ) 12}}
 {{end}}
 
-{{$channel := $LogChannel}}
-{{if .Channel.ID}}
-    {{$channel = .Channel.ID}}
-{{end}}
-
-{{$UnmuteDM := cembed
+{{$WarnDM := cembed
             "author" (sdict "icon_url" (.User.AvatarURL "1024") "name" (print .User.String " (ID " .User.ID ")"))
-            "description" (print "**Server:** " .Guild.Name "\n**Action:** `Unmute`\n**Reason: **" .Reason ".")
-            "thumbnail" (sdict "url" $icon)
+            "description" (print "**Server:** " .Guild.Name "\n**Action:** `Warn`\n**Reason: **" .Reason ".")
+            "thumbnail" (sdict "url" (print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon ".gif"))
             "footer" (sdict "text" " ")
             "timestamp" currentTime
             "color" 3553599
             }}
-{{sendDM $UnmuteDM}}
+{{sendDM $WarnDM}}
 
 {{$Log := cembed
             "author" (sdict "icon_url" (.Author.AvatarURL "1024") "name" (print .Author.String " (ID " .Author.ID ")"))
-            "description" (print ":loud_sound: **Unmute:** *" .User.Username "#" .User.Discriminator "* `(ID " .User.ID ")`\n:receipt: **Channel:** <#" $channel ">\n:page_facing_up: **Reason:** " .Reason "\n:clock12: **Time:** " ( joinStr " " (( currentTime.Add 0).Format "15:04 GMT")))
+            "description" (print ":warning: **Warned:** *" .User.Username "#" .User.Discriminator "* `(ID " .User.ID ")`\n:receipt: **Channel:** <#" .Channel.ID ">\n:page_facing_up: **Reason:** " .Reason "\n:clock12: **Time:** " ( joinStr " " (( currentTime.Add 0).Format "15:04 GMT")))
             "thumbnail" (sdict "url" (.User.AvatarURL "256"))
-            "color" 6473311
+            "color" 16556627
             }}
 {{sendMessage $LogChannel $Log}}
