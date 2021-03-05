80777da36edfb84aec23bfb5090c45499134217f
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-03-05 23:51:06 +0000

    (fix)Whole thing

--- Moderation/Unmute.cc.go
+++ Moderation/Unmute.cc.go
@@ -16,11 +16,23 @@ MIT License
 {{if gt ( toInt ( currentTime.UTC.Format "15" ) ) 12}}
 {{end}}
 
+{{$icon := ""}}
+{{$name := printf "%s (%d)" .Guild.Name .Guild.ID}}
+{{if .Guild.Icon}}
+	{{$ext := "webp"}}
+	{{if eq (slice .Guild.Icon 0 2) "a_"}} {{$ext = "gif"}} {{end}}
+	{{$icon = printf "https://cdn.discordapp.com/icons/%d/%s.%s" .Guild.ID .Guild.Icon $ext}}
+{{end}}
+
+{{$channel := $LogChannel}}
+{{if .Channel.ID}}
+    {{$channel = .Channel.ID}}
+{{end}}
+
 {{$WarnDM := cembed
             "author" (sdict "icon_url" (.User.AvatarURL "1024") "name" (print .User.String " (ID " .User.ID ")"))
             "description" (print "**Server:** " .Guild.Name "\n**Action:** `Warn`\n**Reason: **" .Reason ".")
-            "thumbnail" (sdict "url" (print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon ".gif"))
-            "footer" (sdict "text" " ")
+            "thumbnail" (sdict "url" $icon)
             "timestamp" currentTime
             "color" 3553599
             }}
@@ -28,8 +40,8 @@ MIT License
 
 {{$Log := cembed
             "author" (sdict "icon_url" (.Author.AvatarURL "1024") "name" (print .Author.String " (ID " .Author.ID ")"))
-            "description" (print ":warning: **Warned:** *" .User.Username "#" .User.Discriminator "* `(ID " .User.ID ")`\n:receipt: **Channel:** <#" .Channel.ID ">\n:page_facing_up: **Reason:** " .Reason "\n:clock12: **Time:** " ( joinStr " " (( currentTime.Add 0).Format "15:04 GMT")))
+            "description" (print ":loud_sound: **Unmuted:** *" .User.Username "#" .User.Discriminator "* `(ID " .User.ID ")`\n:receipt: **Channel:** <#" $Channel ">\n:page_facing_up: **Reason:** " .Reason "\n:clock12: **Time:** " ( joinStr " " (( currentTime.Add 0).Format "15:04 GMT")))
             "thumbnail" (sdict "url" (.User.AvatarURL "256"))
-            "color" 16556627
+            "color" 6473311
             }}
 {{sendMessage $LogChannel $Log}}
