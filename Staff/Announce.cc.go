6dd506f01e6bed0f84a4ad952c6f8bbdfe995ba3
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-04-11 15:32:40 +0100

    (fix)Added support  for all server thumbnail

--- Staff/Announce.cc.go
+++ Staff/Announce.cc.go
@@ -15,13 +15,21 @@ MIT License
 
 {{/* Only edit below if you know what you're doing (: rawr */}}
 
+{{$icon := ""}}
+{{$name := printf "%s (%d)" .Guild.Name .Guild.ID}}
+{{if .Guild.Icon}}
+    {{$ext := "webp"}}
+    {{if eq (slice .Guild.Icon 0 2) "a_"}} {{$ext = "gif"}} {{end}}
+    {{$icon = printf "https://cdn.discordapp.com/icons/%d/%s.%s" .Guild.ID .Guild.Icon $ext}}
+{{end}}
+
 {{sendMessageNoEscape $announcementChannel (complexMessage
     "content" (mentionRoleID $announcementRole)
         "embed" (cembed
             "title" (print .User.Username " Has made an announcement!")
                 "url" (print "https://discord.com/channels/" .Guild.ID "/" $announcementChannel)
             "description" (print .StrippedMsg)
-            "thumbnail" (sdict "url" (print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon ".gif"))
+            "thumbnail" (sdict "url" $icon)
             "image" (sdict "url" "https://cdn.discordapp.com/attachments/784132357002625047/789191521689534494/1.png")
             "color" 16761035 ))
             }}
