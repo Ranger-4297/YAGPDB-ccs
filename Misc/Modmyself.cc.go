3285cf9b2e9d53ca0b7b3534ca8f16e79b22d939
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-04-15 02:01:32 +0100

    (update)Fixed Errors

--- Misc/Modmyself.cc.go
+++ Misc/Modmyself.cc.go
@@ -1,8 +1,7 @@
 {{/*
-        Made by Ranger (765316548516380732)
-
-    Trigger Type: `Command`
-    Trigger: `Modmyself`
+        Made by Ranger (779096217853886504)
+    Trigger Type: `RegEx`
+    Trigger: `\A[^a-zA-Z0-9]*\-[​‏]*?m[​‏]*o[​‏]*d[​‏]*m[​‏]*y[​‏]*s[​‏]*e[​‏]*[lI][​‏]*f`
 ©️ Dynamic 2021
 MIT License
 */}}
@@ -11,17 +10,15 @@ MIT License
 {{$serverLink := "https://discord.gg/ekMQH384KC"}} {{/* Your servers invite link */}}
 {{/* Configuration values end */}}
 
-{{/* Only edit below if you know what you're doing except for the 21st line. Add your staff application link at the end. (: rawr */}}
+{{/* Only edit below if you know what you're doing (: rawr */}}
 
 {{$random := randInt 4}}
 {{if le $random 0}}
-    {{execAdmin "warn" .User.ID "you fookin donut, you modded yourself"}}
+    {{execAdmin "warn" .User.ID "you fuckin' donut, you modded yourself"}}
     {{else if le $random 1}}
-        {{execAdmin "mute" .User.ID (randInt 61) "you fookin donut, you modded yourself"}}
-        {{else if le $random 2}}
-            {{execAdmin "kick" .User.ID (print "you fookin donut, you modded yourself :/ rejoin at [" .Server.Name "](" $serverLink ")")}}
-            {{else if le $random 3}}
-                {{execAdmin "ban" .User.ID (print "you fookin donut, you modded yourself\n:/rejoin when your ban has timedout. (Yes. You have to wait) at [" .Server.Name "](" $serverLink ")") "-d" (print (randInt 61) "mins")}}
-        {{end}}
-    {{end}}
+    {{execAdmin "mute" .User.ID (randInt 61) "you fuckin' donut, you modded yourself"}}
+    {{else if le $random 2}}
+     {{execAdmin "kick" .User.ID (print "you fuckin' donut, you modded yourself :/ rejoin at [" .Server.Name "](" $serverLink ")")}}
+     {{else if le $random 3}}
+     {{execAdmin "ban" .User.ID (print "you fuckin' donut, you modded yourself :/ rejoin at [" .Server.Name "](" $serverLink ")") "-d" (print (randInt 61) "mins")}}
 {{end}}
