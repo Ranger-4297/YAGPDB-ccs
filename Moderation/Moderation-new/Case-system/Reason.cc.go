157d7e8a7d0f6a217013f04e11d69b813201e6ff
Rhyker <a.rhykerw@gmail.com>
2021-10-26 05:10:26 +0100

    Adds updates to edit the log aswell

--- Moderation/Moderation-new/Case-system/Reason.cc.go
+++ Moderation/Moderation-new/Case-system/Reason.cc.go
@@ -36,12 +36,22 @@ MIT License
     {{$reason := ""}}
     {{$userid := ""}}
     {{$action := ""}}
+    {{$msgid := ""}}
+    {{$channel := ""}}
+    {{$channel2 := ""}}
+    {{$logs := ""}}
+    {{$time := ""}}
     {{$b := ($args.Get 0)}}
     {{with (dbGet $b "viewcase")}}	
         {{$a = sdict .Value}}
         {{$reason = $a.reason}}
         {{$userid = $a.userid}}
         {{$action = $a.action}}
+        {{$msgid = $a.msgid}}
+        {{$channel = $a.channel}}
+        {{$channel2 = $a.channel2}}
+        {{$logs = $a.logs}}
+        {{$time = $a.time}}
         {{if ($args.Get 1)}}
             {{$newreason := (joinStr " " (slice $.CmdArgs 1))}}
             {{$Response := sendMessage nil (cembed
@@ -53,8 +63,14 @@ MIT License
             {{$sdict := (dbGet $b "viewcase").Value}}
             {{$sdict.Set "reason" $newreason}}
             {{dbSet $b "viewcase" $sdict}}
-            {{dbSet $b $userid (print "Case # **" $b "**\t\t**| " (dbGet $b "cases").Value " Reason:** `" $newreason "`")}}
+            {{if dbGet $b $userid}}
+                {{dbSet $b $userid (print "Case # **" $b "**\t\t**| " (dbGet $b "cases").Value " Reason:** `" $newreason "`")}}
+            {{end}}
+            {{$embed := index (getMessage $channel $msgid).Embeds 0| structToSdict}}
+            {{$embed.Set "description" (print "<:TextChannel:800978104105304065> **Case number:** " $b "\n<:Management:788937280508657694> **Who:** <@!" $userid "> `(ID " $userid ")`\n<:Metadata:788937280508657664> **Action:** `Warn`\n<:Assetlibrary:788937280554926091> **Channel:** <#" $channel2 ">\n<:Manifest:788937280579698728> **Reason:** " $newreason "\n<:Edit:800978104272683038> **Message Logs:** [Click Here](" $logs ")\n:clock12: **Time:** <t:" $time ":f>")}}
+            {{editMessage $channel $msgid (cembed $embed)}}
         {{else}}
+            
         {{sendMessage nil (cembed
             "author" (sdict "icon_url" (.User.AvatarURL "1024") "name" .User.Username)
             "description" (print "Please specify a reason")
