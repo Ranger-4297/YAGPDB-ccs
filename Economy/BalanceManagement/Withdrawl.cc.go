2779f73d0035738a0f2a2702d8d843deebd5666a
Rhyker <a.rhykerw@gmail.com>
2021-12-06 23:13:16 +0000

    Adds custom withdrawls

--- Economy/BalanceManagement/Withdrawl.cc.go
+++ Economy/BalanceManagement/Withdrawl.cc.go
@@ -9,6 +9,8 @@ MIT License
 
 {{/* Only edit below if you know what you're doing (: rawr */}}
 
+{{$args := parseArgs 1 "deposit <Amount:Number/All>" (carg "string" "Amount")}}
+{{$amount := ($args.Get 0)}}
 {{$a := ""}}
 {{$cash := ""}}
 {{$bank := ""}}
@@ -24,18 +26,55 @@ MIT License
 		{{$a = sdict .Value}}
 		{{$cash = $a.cash}}
 		{{$bank = $a.bank}}
-		{{$newCash := (add (toInt $bank) (toInt $cash))}}
-		{{$withdrawEmbed := (cembed
+		{{$newCash := ""}}
+		{{if eq $amount "all"}}
+			{{$newCash = (add (toInt $bank) (toInt $cash))}}
+			{{$withdrawEmbed := (cembed
             "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "128"))
             "description" (print "You withdrew " $EconomySymbol $bank " from your bank!")
             "color" 0x00ff7b
             "timestamp" currentTime
             )}}
-		{{sendMessage nil $withdrawEmbed}}
-		{{$sdict := (dbGet .User.ID "EconomyInfo").Value}}
-		{{$sdict.Set "bank" (toInt "0")}}
-		{{dbSet $b "EconomyInfo" $sdict}}
-		{{$sdict.Set "cash" $newCash}}
-		{{dbSet $b "EconomyInfo" $sdict}}
+			{{sendMessage nil $withdrawEmbed}}
+			{{$sdict := (dbGet .User.ID "EconomyInfo").Value}}
+			{{$sdict.Set "bank" (toInt "0")}}
+			{{dbSet $b "EconomyInfo" $sdict}}
+			{{$sdict.Set "cash" $newCash}}
+			{{dbSet $b "EconomyInfo" $sdict}}
+		{{else if (toInt $amount)}}
+			{{if gt (toInt $amount) $bank}}
+				{{$errorEmbed := (cembed
+                            "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "128"))
+                            "description" (print "You're unable to withdraw more than you have in your bank")
+                            "color" 0x00ff8b
+                            "timestamp" currentTime
+                )}}
+                {{sendMessage nil $errorEmbed}}
+			{{else}}
+				{{$moneyToWithdraw := (toInt $amount)}}
+				{{$newCash = (add $cash $amount)}}
+				{{$newBank := (sub $bank $amount)}}
+				{{$withdrawEmbed := (cembed
+                            "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "128"))
+                            "description" (print "You withdrew " $EconomySymbol $moneyToWithdraw " from your bank!")
+                            "color" 0x00ff7b
+                            "timestamp" currentTime
+                )}}
+                {{sendMessage nil $withdrawEmbed}}
+                {{$sdict := (dbGet .User.ID "EconomyInfo").Value}}
+                {{$sdict.Set "bank" $newBank}}
+                {{dbSet $b "EconomyInfo" $sdict}}
+                {{$sdict.Set "cash" $newCash}}
+                {{dbSet $b "EconomyInfo" $sdict}}
+			{{end}}
+		{{else}}
+			{{$errorEmbed := (cembed
+            "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "128"))
+            "description" (print "You're unable to withdraw this value, check that you used a valid cash amount or all")
+            "color" 0x00ff8b
+            "timestamp" currentTime
+            )}}
+            {{sendMessage nil $errorEmbed}}
+		{{end}}
 	{{end}}
 {{end}}
