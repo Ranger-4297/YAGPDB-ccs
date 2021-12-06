26085a2e390cb92b8b2ec8c7ba0edd333543a73c
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-12-06 14:05:36 +0000

    Update setfailrate.cc.go

--- Economy/untested/setfailrate.cc.go
+++ Economy/untested/setfailrate.cc.go
@@ -2,24 +2,24 @@
         Made by Ranger (765316548516380732)
 
     Trigger Type: `Command`
-    Trigger: `set-failrate`
+    Trigger: `setFailRate`
 ©️ Ranger 2021
 MIT License
 */}}
 
 {{/* Only edit below if you know what you're doing (: rawr */}}
 
-{{$args := parseArgs 1 "set-failrate <value>" (carg "int" "Value")}}
+{{$args := parseArgs 1 "setFailRate <value>" (carg "int" "Value")}}
 {{$newFailRate := $args.Get 0}}
 
-{{$failrate := ""}}
+{{$failRate := ""}}
 {{with (dbGet 0 "EconomySettings")}}
     {{$a = sdict .Value}}
-    {{$failrate = $a.failRate}}
+    {{$failRate = $a.failRate}}
     {{if lt $newFailRate 20}}
         {{$errorEmbed := (cembed
             "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "1020"))
-            "description" (print "You cannot set your failrate below 20%")
+            "description" (print "You cannot set your failRate below 20%")
             "color" 0x00ff8b
             "timestamp" currentTime
             )}}
@@ -27,13 +27,13 @@ MIT License
     {{else}}
         {{$updateEmbed := (cembed
             "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "1020"))
-            "description" (print "You set your failrate to " $newfailrate "% from " $failrate "%")
+            "description" (print "You set your failRate to " $newfailRate "% from " $failRate "%")
             "color" 0x00ff8b
             "timestamp" currentTime
             )}}
         {{sendMessage nil $updateEmbed}}
         {{$sdict := (dbGet 0 "EconomySettings").Value}}
-        {{$sdict.Set "failrate" $newFailRate}}
+        {{$sdict.Set "failRate" $newFailRate}}
         {{dbSet 0 "EconomySettings" $sdict}}
         {{end}}
 {{end}}
