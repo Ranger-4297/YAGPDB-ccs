0d55c1aceb27792b06db8ca634e74351aa7d3885
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-12-07 09:46:36 +0000

    Update SetStartBalance.cc.go

--- Economy/Settings/SetStartBalance.cc.go
+++ Economy/Settings/SetStartBalance.cc.go
@@ -12,13 +12,15 @@ MIT License
 {{$args := parseArgs 1 "setStartBalance Symbol" (carg "string" "Value")}}
 {{$newStartBalance := (toInt $args.Get 0)}}
 {{$startBalance := ""}}
+{{$economySymbol := ""}}
 {{$a := ""}}
 {{with (dbGet 0 "EconomySettings")}}
     {{$a = sdict .Value}}
     {{$startBalance = $a.startBalance}}
+    {{$economySymbol = $a.economySymbol}}
     {{$updateEmbed := (cembed
             "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "128"))
-            "description" (print "You set the server start-balance to symbol to " $newStartBalance " from " $startBalance)
+            "description" (print "You set the server start-balance to " $newStartBalance " from " $startBalance)
             "color" 0x00ff8b
             "timestamp" currentTime
             )}}
