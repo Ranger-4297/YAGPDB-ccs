7b0f9f84c89c37a02a9348acc482951de83e22a3
Rhyker <a.rhykerw@gmail.com>
2021-12-08 01:26:03 +0000

    Moves dir and minor fix to command

--- Economy/BalanceManagement/JoinMessage.cc.go
+++
@@ -1,9 +0,0 @@
-{{/* Add this to your join message to set the default starter-balance */}}
-
-{{$startBalance := ""}}
-{{$a :=""}}
-{{with (dbGet 0 "EconomySettings")}}
-	{{$a = sdict .Value}}
-	{{$startBalance = (toInt $a.startBalance)}}
-	{{dbSet .User.ID "EconomyInfo" (sdict "cash" $startBalance "bank" 0}}
-{{end}}
