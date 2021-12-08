d2bb9ade551bd1f13a730bbbf263c5ac05b63706
Rhyker <a.rhykerw@gmail.com>
2021-12-08 08:18:18 +0000

    Add viewsettings

---
+++ Economy/Settings/Untested/ViewSettings.cc.go
@@ -0,0 +1,34 @@
+{{/*
+        Made by Ranger (765316548516380732)
+
+    Trigger Type: `Command`
+    Trigger: `viewSettings`
+©️ Ranger 2021
+MIT License
+*/}}
+
+{{$a := ""}}
+{{$min := ""}}
+{{$max := ""}}
+{{$failRate := ""}}
+{{$symbol := ""}}
+{{$startBalance := ""}}
+{{$settingsEmbed := (cembed
+            "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "128"))
+            "description" (print "No settings data found")
+            "timestamp" currentTime
+            )}}
+{{with (dbGet 0 "EconomySettings")}}
+    {{$a = sdict .Value}}
+    {{$min = $a.min}}
+    {{$max = $a.max}}
+    {{$failRate = $a.failRate}}
+    {{$symbol = $a.symbol}}
+    {{$startBalance = $a.startBalance}}
+    {{$settingsEmbed = (cembed
+            "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "128"))
+            "description" (print "Min: `" $min "`\nMax: `" $max "`\nfailRate: `" $failRate "`\nEconomy symbol: `" $symbol "`\nstarBalance: `" $symbol $startBalance "`")
+            "timestamp" currentTime
+            )}}
+{{end}}
+{{sendMessage nil $settingsEmbed}}
