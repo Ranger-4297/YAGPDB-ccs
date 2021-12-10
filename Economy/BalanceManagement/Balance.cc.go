11c5038254f5a418dfda4f401c4db46876deeb24
Rhyker <a.rhykerw@gmail.com>
2021-12-10 16:12:33 +0000

    Optional user argument "bal [user]"

--- Economy/BalanceManagement/Balance.cc.go
+++ Economy/BalanceManagement/Balance.cc.go
@@ -9,24 +9,33 @@ MIT License
 
 {{/* Only edit below if you know what you're doing (: rawr */}}
 
-{{$a := ""}}
-{{$cash := ""}}
-{{$bank := ""}}
-{{$symbol := ""}}
-{{$b := .User.ID}}
-{{if not (dbGet $b "EconomyInfo")}}
-    {{dbSet .User.ID "EconomyInfo" (sdict "cash" 0 "bank" 0)}}
+{{$args := parseArgs 0 "bal [Member:Mention/ID]" (carg "member" "Member")}}
+{{$errorColor := 0xFF0000}}
+{{$member := ""}}
+{{if ($args.Get 0).User}}
+    {{$member = ($args.Get 0).User}}
+{{else if not ($args.Get 0)}}
+    {{$member = .User}}
+{{else}}
+    {{$errorEmbed := (cembed
+            "description" (print "Invalid user argument passed`")
+            "color" $errorColor
+            )}}
+    {{sendMessage nil $errorEmbed}}
+{{end}}
+{{if not (dbGet $member.ID "EconomyInfo")}}
+    {{dbSet $member.ID "EconomyInfo" (sdict "cash" 0 "bank" 0)}}
 {{end}}
 {{with (dbGet 0 "EconomySettings")}}
-	{{$a = sdict .Value}}
-	{{$symbol = $a.symbol}}
-	{{with (dbGet $b "EconomyInfo")}}
+	{{$a := sdict .Value}}
+	{{$symbol := $a.symbol}}
+	{{with (dbGet $member.ID "EconomyInfo")}}
         {{$a = sdict .Value}}
-		{{$cash = $a.cash}}
-		{{$bank = $a.bank}}
+		{{$cash := $a.cash}}
+		{{$bank := $a.bank}}
 		{{$balanceEmbed := (cembed
-            "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "128"))
-            "description" (print "Your balance")
+            "author" (sdict "name" $member.Username "icon_url" ($member.AvatarURL "128"))
+            "description" (print $member.Mention "'s balance")
             "fields" (cslice 
                 (sdict "name" "Cash" "value" (print $symbol (toInt $cash)) "inline" true)
                 (sdict "name" "Bank" "value" (print $symbol (toInt $bank)) "inline" true)
