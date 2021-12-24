e039c2c955f5e66db1078c319a11dc033df15b05
Rhyker <a.rhykerw@gmail.com>
2021-12-24 05:34:32 +0000

    Add informational

---
+++ Economy/V3/Informational/balance.cc.go
@@ -0,0 +1,65 @@
+{{/*
+        Made by Ranger (765316548516380732)
+
+    Trigger Type: `Regex`
+    Trigger: `\A(-|<@!?204255221017214977>\s*)(bal(ance)?|wallet|money)(\s+|\z)`
+
+    ©️ Ranger 2020-Present
+    GNU, GPLV3 License
+    Repository: https://github.com/Ranger-4297/YAGPDB-ccs
+*/}}
+
+
+{{/* Only edit below if you know what you're doing (: rawr */}}
+
+{{/* Initiates variables */}}
+{{$user := .User}}
+{{$successColor := 0x00ff7b}}
+{{$errorColor := 0xFF0000}}
+
+{{/* Balance, Deposit, Withdraw */}}
+
+{{/*
+If the user isn't in the economy database 
+It'll automatically add them
+--
+If there is no setting values
+You'll be asked to set it up with default values
+You can change these later
+*/}}
+
+{{/* Response */}}
+{{$embed := sdict}}
+{{$embed.Set "author" (sdict "name" .User.Username "icon_url" (.User.AvatarURL "1024"))}}
+{{$embed.Set "timestamp" currentTime}}
+{{with (dbGet 0 "EconomySettings")}}
+    {{$a := sdict .Value}}
+    {{$symbol := $a.symbol}}
+    {{with $.CmdArgs}}
+        {{$newUser := (index . 0) | userArg}}
+        {{if $newUser}}
+            {{$user = $newUser}}
+        {{end}}
+    {{end}}
+    {{if not (dbGet $user.ID "EconomyInfo")}}
+        {{dbSet $user.ID "EconomyInfo" (sdict "cash" 200 "bank" 0)}}
+    {{end}}
+    {{with (dbGet $user.ID "EconomyInfo")}}
+        {{$a = sdict .Value}}
+        {{$cash := $a.cash | toInt}}
+        {{$bank := $a.bank | toInt}}
+        {{$net := $cash | add $bank}}
+        {{$embed.Set "author" (sdict "name" $user.Username "icon_url" ($user.AvatarURL "128"))}}
+        {{$embed.Set "description" (print $user.Mention "'s balance")}}
+        {{$embed.Set "fields" (cslice 
+            (sdict "name" "Cash" "value" (print $symbol $cash) "inline" true)
+            (sdict "name" "Bank" "value" (print $symbol $bank) "inline" true)
+            (sdict "name" "Networth" "value" (print $symbol $net) "inline" true))}}
+        {{$embed.Set "color" $successColor}}
+        {{$embed.Set "timestamp" currentTime}}
+    {{end}}
+{{else}}
+    {{$embed.Set "description" (print "No database found.\nPlease set it up with the default values using `" $.Cmd " default`")}}
+    {{$embed.Set "color" $errorColor}}
+{{end}}
+{{sendMessage nil (cembed $embed)}}
