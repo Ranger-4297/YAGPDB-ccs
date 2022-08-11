81a840244c58d38100b67629d3e821f918dfc5ad
Rhyker Wells <a.rhykerw@gmail.com>
2022-08-11 15:45:23 +0100

    V4(Join,Set,View)

---
+++ Economy/V4/Settings/joinMessage.cc.go
@@ -0,0 +1,19 @@
+{{/*
+        Made by Ranger (765316548516380732)
+
+    Trigger Type: `Join message in channel`
+
+    ©️ Ranger 2020-Present
+    GNU, GPLV3 License
+    Repository: https://github.com/Ranger-4297/YAGPDB-ccs
+*/}}
+
+
+{{/* Only edit below if you know what you're doing (: rawr */}}
+
+{{/* Response */}}
+{{with (dbGet 0 "EconomySettings")}}
+    {{$a := sdict .Value}}
+    {{$startBalance := (toInt $a.startBalance)}}
+    {{dbSet .User.ID "EconomyInfo" (sdict "cash" $startBalance "bank" 0)}}
+{{end}}
