e92f37489d9ff90ca3b6ee0959ceb402f02dbd16
Rhyker Wells <a.rhykerw@gmail.com>
2022-08-17 13:25:08 +0100

    Feat(create-item): Create item command

---
+++ Economy/V4/Shop/create-item.cc.go
@@ -0,0 +1,30 @@
+{{/*
+        Made by Ranger (765316548516380732)
+
+    Trigger Type: `Regex`
+    Trigger: `\A(-|<@!?204255221017214977>\s*)((create|new)-?item)(\s+|\z)`
+
+    ©️ Ranger 2020-Present
+    GNU, GPLV3 License
+    Repository: https://github.com/Ranger-4297/YAGPDB-ccs
+*/}}
+
+
+{{/* Only edit below if you know what you're doing (: rawr */}}
+
+{{/* Initiates variables */}}
+{{$successColor := 0x00ff7b}}
+{{$errorColor := 0xFF0000}}
+
+{{/* Create item */}}
+
+{{/* Response */}}
+{{$embed := sdict}}
+{{$embed.Set "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "1024"))}}
+{{$embed.Set "timestamp" currentTime}}
+{{with (dbGet 0 "EconomySettings")}}
+{{else}}
+    {{$embed.Set "description" (print "No `Settings` database found.\nPlease set it up with the default values using `" $prefix "set default`")}}
+    {{$embed.Set "color" $errorColor}}
+{{end}}
+{{sendMessage nil (cembed $embed)}}
