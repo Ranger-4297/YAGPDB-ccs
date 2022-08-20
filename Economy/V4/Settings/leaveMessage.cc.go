27d983525c97708deb6abf679859ca416aab5a28
Rhyker Wells <a.rhykerw@gmail.com>
2022-08-20 23:30:39 +0100

    feat: Adds leave message: Deleted users economy data upon leaving guild

---
+++ Economy/V4/Settings/leaveMessage.cc.go
@@ -0,0 +1,15 @@
+{{/*
+        Made by Ranger (765316548516380732)
+
+    Trigger Type: `Leave message in channel`
+
+    ©️ Ranger 2020-Present
+    GNU, GPLV3 License
+    Repository: https://github.com/Ranger-4297/YAGPDB-ccs
+*/}}
+
+
+{{/* Only edit below if you know what you're doing (: rawr */}}
+
+{{/* Response */}}
+{{dbDel .User.ID "EconomyInfo"}}
