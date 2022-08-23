0a94bfd3ba5573d802c0727dad774cc34a5d3505
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2022-08-23 17:26:19 +0100

    MM-merge (#10)

--- Economy/V4/MoneyManagement/add-removeMoney.cc.go
+++ Economy/V4/MoneyManagement/add-removeMoney.cc.go
@@ -1,23 +1,23 @@
-    {{/*
-            Made by Ranger (765316548516380732)
+{{/*
+		Made by Ranger (765316548516380732)
 
-        Trigger Type: `Regex`
-        Trigger: `\A(-|<@!?204255221017214977>\s*)(add-?money|inc(crease)?-?money)(\s+|\z)`
+	Trigger Type: `Regex`
+	Trigger: `\A(-|<@!?204255221017214977>\s*)(add-?money|inc-?money|money-?add|remove-?money|dec(rease)?-?money)(\s+|\z)`
 
-        ©️ Ranger 2020-Present
-        GNU, GPLV3 License
-        Repository: https://github.com/Ranger-4297/YAGPDB-ccs
-    */}}
+    ©️ Ranger 2020-Present
+    GNU, GPLV3 License
+    Repository: https://github.com/Ranger-4297/YAGPDB-ccs
+*/}}
 
 
-    {{/* Only edit below if you know what you're doing (: rawr */}}
+{{/* Only edit below if you know what you're doing (: rawr */}}
 
 {{/* Initiates variables */}}
 {{$successColor := 0x00ff7b}}
 {{$errorColor := 0xFF0000}}
-{{$prefix := index (reFindAllSubmatches `.*?: \x60(.*)\x60\z` (execAdmin "Prefix")) 0 1}}
+{{$prefix := index (reFindAllSubmatches `.*?: \x60(.*)\x60\z` (execAdmin "Prefix")) 0 1 }}
 
-{{/* Adds money to given user */}}
+{{/* Removes money from given user */}}
 
 {{/* Response */}}
 {{$embed := sdict}}
@@ -29,34 +29,42 @@
         {{$a := sdict .Value}}
         {{$symbol := $a.symbol}}
         {{with $.CmdArgs}}
-            {{if (index . 0)}}
-                {{if (index . 0) | getMember}}
-                    {{$user := (index . 0) | getMember}}
-                    {{$receivingUser := $user.User.ID}}
-                    {{if not (dbGet $receivingUser "EconomyInfo")}}
-                        {{dbSet $receivingUser "EconomyInfo" (sdict "cash" 200 "bank" 0)}}
+            {{if index . 0}}
+                {{if index . 0 | getMember}}
+                    {{$user := getMember (index . 0)}}
+                    {{$user = $user.User.ID}}
+                    {{if not (dbGet $user "EconomyInfo")}}
+                        {{dbSet $user "EconomyInfo" (sdict "cash" 200 "bank" 0)}}
                     {{end}}
                     {{if gt (len $.CmdArgs) 1}}
-                        {{$moneyDestination := (index . 1) | lower}}
+                        {{$moneyDestination := (lower (index . 1))}}
                         {{if eq $moneyDestination "cash" "bank"}}
-                            {{with (dbGet $receivingUser "EconomyInfo")}}
+                            {{with (dbGet $user "EconomyInfo")}}
                                 {{$a = sdict .Value}}
-                                {{$receivingBalance := $a.Get $moneyDestination}}
+                                {{$balance := $a.Get $moneyDestination}}
                                 {{if gt (len $.CmdArgs) 2}}
                                     {{$amount := (index $.CmdArgs 2)}}
                                     {{if (toInt $amount)}}
                                         {{if gt (toInt $amount) 0}}
-                                            {{$receivingNewBalance := $receivingBalance | add $amount}}
-                                            {{$embed.Set "description" (print "You added " $symbol (humanizeThousands $amount) " to <@!" $receivingUser ">'s " $moneyDestination "\nThey now have " $symbol (humanizeThousands $receivingNewBalance) " in their " $moneyDestination "!")}}
-                                            {{$embed.Set "color" $successColor}}
-                                            {{$a.Set $moneyDestination $receivingNewBalance}}
-                                            {{dbSet $receivingUser "EconomyInfo" $a}}
+                                            {{if (reFind `remove-?money|dec(rease)?-?money` $.Cmd)}}
+                                                {{$newBalance := $amount | sub $balance}}
+                                                {{$embed.Set "description" (print "You removed " $symbol (humanizeThousands $amount) " from <@!" $user ">'s " $moneyDestination "\nThey now have " $symbol (humanizeThousands $newBalance) " in their " $moneyDestination "!")}}
+                                                {{$embed.Set "color" $successColor}}
+                                                {{$a.Set $moneyDestination $newBalance}}
+                                                {{dbSet $user "EconomyInfo" $a}}
+                                            {{else if (reFind `add-?money|inc(crease)?-?money` $.Cmd)}}
+                                                {{$newBalance := $balance | add $amount}}
+                                                {{$embed.Set "description" (print "You added " $symbol (humanizeThousands $amount) " to <@!" $user ">'s " $moneyDestination "\nThey now have " $symbol (humanizeThousands $newBalance   ) " in their " $moneyDestination "!")}}
+                                                {{$embed.Set "color" $successColor}}
+                                                {{$a.Set $moneyDestination $newBalance}}
+                                                {{dbSet $user "EconomyInfo" $a}}
+                                            {{end}}
                                         {{else}}
-                                            {{$embed.Set "description" (print "You're unable to add this value, check that you used a valid number above 1")}}
+                                            {{$embed.Set "description" (print "You're unable to select this value, check that you used a valid number above 1")}}
                                             {{$embed.Set "color" $errorColor}}
                                         {{end}}
                                     {{else}}
-                                        {{$embed.Set "description" (print "You're unable to add this value, check that you used a valid number")}}
+                                        {{$embed.Set "description" (print "You're unable to select this value, check that you used a valid number above 1")}}
                                         {{$embed.Set "color" $errorColor}}
                                     {{end}}
                                 {{else}}
