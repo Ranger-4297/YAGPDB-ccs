b202f264a429ca051d5f7978755a2eb81a6b3d4c
Rhyker <a.rhykerw@gmail.com>
2022-12-29 18:39:46 +0000

    feat(timely): Initial commit. Add timely + base

---
+++ Economy/V6/Plugins/Timely/timely.cc.go
@@ -0,0 +1,30 @@
+{{/*
+		Made by Ranger (765316548516380732)
+
+	Trigger Type: `Regex`
+	Trigger: `\A(-|<@!?204255221017214977>\s*)(daily|weekly|monthly)(\s+|\z)`
+
+	©️ Ranger 2020-Present
+	GNU, GPLV3 License
+	Repository: https://github.com/Ranger-4297/YAGPDB-ccs
+*/}}
+
+
+{{/* Only edit below if you know what you're doing (: rawr */}}
+
+{{/* Initiates variables */}}
+{{$userID := .User.ID}}
+{{$successColor := 0x00ff7b}}
+{{$errorColor := 0xFF0000}}
+{{$prefix := index (reFindAllSubmatches `.*?: \x60(.*)\x60\z` (execAdmin "Prefix")) 0 1}}
+
+
+{{/* Timely */}}
+
+{{/* Response */}}
+{{with dbGet 0 "EconomySettings"}}
+	{{$a := sdict .Value}}
+{{else}}
+	{{$embed.Set "description" (print "No `Settings` database found.\nPlease set it up with the default values using `" $prefix "set default`")}}
+	{{$embed.Set "color" $errorColor}}
+{{end}}
