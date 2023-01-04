35ec0180a06f3e3ce428831d941b1a3be4b85923
Rhyker <a.rhykerw@gmail.com>
2023-01-04 12:12:53 +0000

    feat(timely): Add support for streaks-consistency rewards.

--- Economy/V6/Plugins/Timely/timely.cc.go
+++ Economy/V6/Plugins/Timely/timely.cc.go
@@ -36,13 +36,21 @@
 	{{end}}
 	{{with (dbGet $userID "EconomyInfo")}}
 		{{$a = sdict .Value}}
+		{{$streaks := $a.streaks}}
 		{{$balance = $a.cash}}
 		{{$cmd := $.Cmd | lower | toString}}
 		{{if eq $cmd "daily"}}
 			{{if not ($cd := dbGet $userID "dCooldown")}}
 				{{dbSetExpire $userID "dCooldown" "cooldown" 86400}}
+				{{$streak := $streaks.daily}}
+				{{$streak = (print "1." $streak)}}
+				{{$daily = mult (toFloat $streak) $daily}}
 				{{$embed.Set "description" (print "You've just claimed your " $symbol $daily " " $cmd"! Come back in " (humanizeDurationSeconds ($cd.ExpiresAt.Sub currentTime)))}}
 				{{$embed.Set "color" $successColor}}
+				{{if not eq $streaks.daily 10}}
+					{{$streaks.Set "daily" (add $streaks.daily 1)}}
+					{{$a.Set "streaks" $streaks}}
+				{{end}}
 				{{$newBalance := add $cash $daily}}
 				{{$a.Set "cash" $newBalance}}
 				{{dbSet $userID "EconomyInfo" $a}}
@@ -53,8 +61,15 @@
 		{{else if eq $cmd "weekly"}}
 			{{if not ($cd := dbGet $userID "wCooldown")}}
 				{{dbSetExpire $userID "wCooldown" "cooldown" 691200}}
+				{{$streak := $streaks.weekly}}
+				{{$streak = (print "1." $streak)}}
+				{{$weekly = mult (toFloat $streak) $weekly}}
 				{{$embed.Set "description" (print "You've just claimed your " $symbol $weekly " " $cmd"! Come back in " (humanizeDurationSeconds ($cd.ExpiresAt.Sub currentTime)))}}
 				{{$embed.Set "color" $successColor}}
+				{{if not eq $streaks.weekly 10}}
+					{{$streaks.Set "weekly" (add $streaks.weekly 1)}}
+					{{$a.Set "streaks" $streaks}}
+				{{end}}
 				{{$newBalance := add $cash $weekly}}
 				{{$a.Set "cash" $newBalance}}
 				{{dbSet $userID "EconomyInfo" $a}}
@@ -65,8 +80,15 @@
 		{{else if eq $cmd "monthly"}}
 			{{if not ($cd := dbGet $userID "mCooldown")}}
 				{{dbSetExpire $userID "mCooldown" "cooldown" 2716400}}
+				{{$streak := $streaks.monthly}}
+				{{$streak = (print "1." $streak)}}
+				{{$monthly = mult (toFloat $streak) $monthly}}
 				{{$embed.Set "description" (print "You've just claimed your " $symbol $monthly " " $cmd"! Come back in " (humanizeDurationSeconds ($cd.ExpiresAt.Sub currentTime)))}}
 				{{$embed.Set "color" $successColor}}
+				{{if not eq $streaks.monthly 10}}
+					{{$streaks.Set "monthly" (add $streaks.monthly 1)}}
+					{{$a.Set "streaks" $streaks}}
+				{{end}}
 				{{$newBalance := add $cash $monthly}}
 				{{$a.Set "cash" $newBalance}}
 				{{dbSet $userID "EconomyInfo" $a}}
