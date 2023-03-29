1bee19a96f0006d695a8b618d52958566049c813
Ranger-4297 <a.rhykerw@gmail.com>
2023-03-29 22:09:42 +0100

    feat(info): Add role field to information embed

--- Economy/V6/Shop/item-info.cc.go
+++ Economy/V6/Shop/item-info.cc.go
@@ -2,7 +2,7 @@
 		Made by Ranger (765316548516380732)
 
 	Trigger Type: `Regex`
-	Trigger: `\A(-|<@!?204255221017214977>\s*)(item-?info)(\s+|\z)`
+	Trigger: `\A(-|<@!?204255221017214977>\s*)(item-?info|view-?item)(\s+|\z)`
 
 	©️ Ranger 2020-Present
 	GNU, GPLV3 License
@@ -36,6 +36,10 @@
 				{{if $items.Get $name}}
 					{{$item := $items.Get (index . 0)}}
 					{{$price := $item.Get "price"}}
+					{{$role := "none"}}
+					{{if ($item.Get "role")}}
+						{{$role = (print "<@&" ($item.Get "role") ">")}}
+					{{end}}
 					{{$qty := ""}}
 					{{if ($item.Get "qty")}}
 						{{$qty = ($item.Get "qty")}}
@@ -49,7 +53,7 @@
 					{{end}}
 					{{$desc := $item.Get "desc"}}
 					{{$embed.Set "title" (print "**Item info**")}}
-					{{$embed.Set "fields" (cslice (sdict "name" "Name" "value" (print $name) "inline" true) (sdict "name" (print "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀") "value" (print "⠀⠀") "inline" true) (sdict "name" "Price" "value" (print $symbol (humanizeThousands $price)) "inline" true) (sdict "name" "Description" "value" (print $desc) "inline" false) (sdict "name" "Quantity" "value" (print $qty)))}}
+					{{$embed.Set "fields" (cslice (sdict "name" "Name" "value" (print $name) "inline" true) (sdict "name" (print "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀") "value" (print "⠀⠀") "inline" true) (sdict "name" "Price" "value" (print $symbol (humanizeThousands $price)) "inline" true) (sdict "name" "Description" "value" (print $desc) "inline" true) (sdict "name" (print "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀") "value" (print "⠀⠀") "inline" true) (sdict "name" "Quantity" "value" (print $qty) "inline" true) (sdict "name" "Role" "value" (print $role)))}}
 					{{$embed.Set "color" $successColor}}
 				{{else}}
 					{{$embed.Set "description" (print "Invalid item argument provided :(\nSyntax is `" $.Cmd " <Name>`")}}
