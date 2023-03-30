c5d63b5911870476f0c1dae45b7ce9e9313bd496
Ranger <a.rhykerw@gmail.com>
2023-03-30 14:35:25 +0100

    feat(settings): Add betMax to set/viewsettings (#19)

--- Economy/Settings/viewSettings.cc.go
+++ Economy/Settings/viewSettings.cc.go
@@ -27,6 +27,7 @@
 	{{$a := sdict .Value}}
 	{{$min := (humanizeThousands $a.min)}}
 	{{$max := (humanizeThousands $a.max)}}
+	{{$betMax := (humanizeThousands $a.betMax)}}
 	{{$symbol := $a.symbol}}
 	{{$incomeCooldown := humanizeDurationSeconds (mult $.TimeSecond $a.incomeCooldown | toDuration)}}
 	{{$workCooldown := humanizeDurationSeconds (mult $.TimeSecond $a.workCooldown | toDuration)}}
@@ -36,9 +37,9 @@
 	{{if (reFind `(<a?:[A-z+]+\:\d{17,19}>)` $symbol)}}
 		{{$symbol = $symbol}}
 	{{else}}
-		{{$symbol = (print $symbol)}}
+		{{$symbol = (print "`" $symbol "`")}}
 	{{end}}
-	{{$embed.Set "description" (print "Min: `" $min "`\nMax: `" $max "`\nSymbol: " $symbol "\nstartBalance: `" $startBalance "`\nincomeCooldown: `" $incomeCooldown "`\nworkCooldown: `" $workCooldown "`\ncrimeCooldown: `" $crimeCooldown "`\nrobCooldown: `" $robCooldown "`")}}
+	{{$embed.Set "description" (print "Min: `" $min "`\nMax: `" $max "`\nbetMax `" $betMax "`\nSymbol: " $symbol "\nstartBalance: `" $startBalance "`\nincomeCooldown: `" $incomeCooldown "`\nworkCooldown: `" $workCooldown "`\ncrimeCooldown: `" $crimeCooldown "`\nrobCooldown: `" $robCooldown "`")}}
 	{{$embed.Set "color" $successColor}}
 {{else}}
 	{{$embed.Set "description" (print "No `Settings` database found.\nPlease set it up with the default values using `" $prefix "set default`")}}
