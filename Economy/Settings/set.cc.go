a24df5f6e28a88f1b474ef6bbb382fd6333b0551
Ranger <a.rhykerw@gmail.com>
2023-04-12 01:45:00 +0100

    Feat: russianRoulette command and functions
    * feat(moneyMaking): Initial commit
    
    Begin implementation of russian roulette
    
    * feat(rr): Check $betMax on bets. Betting "all" assigns $bet to $bal
    
    * feat(set): Set roulette database on default
    
    * feat(rr): Remove debugging. Autostarts on 5 players. Stores payout in a db for users to retrieve later
    
    * feat(rr): Retrieve users winnings. Update TODO
    
    * feat(rr): Move stored user-data from CSLICE to SDICT. Max player count increased from 5>6.
    
    * fix(rr): Misc changes to indexing and embed format
    
    * fix(rr): Fix type comparisons
    
    * feat(rr): Auto-timeout after 5 minutes
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * fix: Remove debugging. Update footer display values
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * fix: Bet can no longer exceed initial roulette bet
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * fix: Database names. Footer display
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * Return money on no-start
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * fix: Save payout on roulette finish
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * refactor: Reduce file size and refactor variable names
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * refactor: Rework database handling for storing payouts
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * fix: Roulette payout moved from cslice to sdict
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * fix: Roulette payout moved from cslice to sdict
    
    Would help if I added a DB call lol
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * fix: Remove code inside range and add db call where neccessary
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * refactor: retrieval
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * fix: variable names
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * fix: Game and and auto-cancel after 7 minutes now assigns bet values to database
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    * fix: collect now works only AFTER a game has ended. Money is deposited on failed games
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>
    
    ---------
    
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>

--- Economy/Settings/set.cc.go
+++ Economy/Settings/set.cc.go
@@ -34,6 +34,7 @@
 				{{$msg.Set "color" $sC}}
 				{{dbSet 0 "EconomySettings" (sdict "min" 200 "max" 500 "betMax" 5000 "symbol" "£" "startBalance" 200 "incomeCooldown" 300 "workCooldown" 7200 "crimeCooldown" 14400 "robCooldown" 21600)}}
 				{{dbSet 0 "store" sdict}}
+				{{dbSet 0 "russianRoulette" sdict}}
 			{{else}}
 				{{with (dbGet 0 "EconomySettings")}}
 					{{$a := sdict .Value}}
