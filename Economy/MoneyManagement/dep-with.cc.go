5a94fe332bbe32b427b4c9d92cd7c1dee0591379
Ranger-4297 <a.rhykerw@gmail.com>
2023-04-14 04:02:21 +0100

    Feat(dep-with):Rework balance system and cleanup
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>

--- Economy/MoneyManagement/dep-with.cc.go
+++ Economy/MoneyManagement/dep-with.cc.go
@@ -13,7 +13,7 @@
 {{/* Only edit below if you know what you're doing (: rawr */}}
 
 {{/* Initiates variables */}}
-{{$user := .User}}
+{{$userID := .User.ID}}
 {{$successColor := 0x00ff7b}}
 {{$errorColor := 0xFF0000}}
 {{$prefix := index (reFindAllSubmatches `.*?: \x60(.*)\x60\z` (execAdmin "Prefix")) 0 1}}
@@ -29,37 +29,31 @@
 	{{with (dbGet 0 "EconomySettings")}}
 		{{$a := sdict .Value}}
 		{{$symbol := $a.symbol}}
+		{{$bankDB := (dbGet 0 "bank").Value}}
+		{{bank := or ($bankDB.Get (toString $userID)) 0 | toInt}}
+		{{$cash := or (dbGet $userID "cash").Value 0 | toInt}}
 		{{if (reFind `deposit|dep` $cmd)}}
 			{{with $.CmdArgs}}
-				{{if not (dbGet $user.ID "EconomyInfo")}}
-					{{dbSet $user.ID "EconomyInfo" (sdict "cash" 200 "bank" 0)}}
-				{{end}}
-				{{with (dbGet $user.ID "EconomyInfo")}}
-					{{$a = sdict .Value}}
-					{{$cash := (toInt $a.cash)}}
-					{{$bank := (toInt $a.bank)}}
-					{{$amount := (index $.CmdArgs 0)}}
-					{{if (toInt $amount)}}
-						{{if gt (toInt $amount) (toInt $cash)}}
-							{{$embed.Set "description" (print "You're unable to deposit more than you have on hand.\nYou currently have " $symbol (humanizeThousands $cash) " on you.")}}
-							{{$embed.Set "color" $errorColor}}
-						{{else}}
-							{{$newCashBalance := $amount | sub $cash}}
-							{{$newBankBalance := $amount | add $bank}}
-							{{$embed.Set "description" (print "You deposited " $symbol (humanizeThousands $amount) " into your bank!")}}
-							{{$embed.Set "color" $successColor}}
-							{{dbSet $user.ID "EconomyInfo" (sdict "cash" $newCashBalance "bank" $newBankBalance)}}
-						{{end}}
-					{{else if eq (lower (toString $amount)) "all"}}
-						{{$newCashBalance := (toInt 0)}}
-						{{$newBankBalance := $bank | add $cash}}
-						{{$embed.Set "description" (print "You deposited " $symbol (humanizeThousands $cash) " into your bank!")}}
-						{{$embed.Set "color" $successColor}}
-						{{dbSet $user.ID "EconomyInfo" (sdict "cash" $newCashBalance "bank" $newBankBalance)}}
-					{{else}}
-						{{$embed.Set "description" (print "Invalid `amount` argument provided.\nCommand syntax is `" $.Cmd " <Amount:Amount>`")}}
+				{{$a = sdict .Value}}
+				{{$amount := (index $.CmdArgs 0)}}
+				{{if (toInt $amount)}}
+					{{if gt (toInt $amount) (toInt $cash)}}
+						{{$embed.Set "description" (print "You're unable to deposit more than you have on hand.\nYou currently have " $symbol (humanizeThousands $cash) " on you.")}}
 						{{$embed.Set "color" $errorColor}}
+					{{else}}
+						{{$embed.Set "description" (print "You deposited " $symbol (humanizeThousands $amount) " into your bank!")}}
+						{{$embed.Set "color" $successColor}}
+						{{$cash = sub $cash $amount}}
+						{{$bank = add $bank $amount}}
 					{{end}}
+				{{else if eq (lower (toString $amount)) "all"}}
+					{{$embed.Set "description" (print "You deposited " $symbol (humanizeThousands $cash) " into your bank!")}}
+					{{$embed.Set "color" $successColor}}
+					{{$cash = (toInt 0)}}
+					{{$bank = add $bank $cash}}
+				{{else}}
+					{{$embed.Set "description" (print "Invalid `amount` argument provided.\nCommand syntax is `" $.Cmd " <Amount:Amount>`")}}
+					{{$embed.Set "color" $errorColor}}
 				{{end}}
 			{{else}}
 				{{$embed.Set "description" (print "No `amount` argument provided.\nCommand syntax is `" $.Cmd " <Amount:Amount>`")}}
@@ -67,41 +61,34 @@
 			{{end}}
 		{{else if (reFind `withdraw|with` $cmd)}}
 			{{with $.CmdArgs}}
-				{{if not (dbGet $user.ID "EconomyInfo")}}
-					{{dbSet $user.ID "EconomyInfo" (sdict "cash" 200 "bank" 0)}}
-				{{end}}
-				{{with (dbGet $user.ID "EconomyInfo")}}
-					{{$a = sdict .Value}}
-					{{$cash := (toInt $a.cash)}}
-					{{$bank := (toInt $a.bank)}}
-					{{$amount := (index $.CmdArgs 0)}}
-					{{if (toInt $amount)}}
-						{{if gt (toInt $amount) (toInt $bank)}}
-							{{$embed.Set "description" (print "You're unable to withdraw more than you have in your bank.\nYou currently have " $symbol (humanizeThousands $bank) " in your bank.")}}
-							{{$embed.Set "color" $errorColor}}
-						{{else}}
-							{{$newCashBalance := $amount | add $cash}}
-							{{$newBankBalance := $amount | sub $bank}}
-							{{$embed.Set "description" (print "You withdrew " $symbol (humanizeThousands $amount) " from your bank!")}}
-							{{$embed.Set "color" $successColor}}
-							{{dbSet $user.ID "EconomyInfo" (sdict "cash" $newCashBalance "bank" $newBankBalance)}}
-						{{end}}
-					{{else if eq (lower (toString $amount)) "all"}}
-						{{$newCashBalance := $bank | add $cash}}
-						{{$newBankBalance := (toInt 0)}}
-						{{$embed.Set "description" (print "You withdrew " $symbol (humanizeThousands $bank) " from your bank!")}}
-						{{$embed.Set "color" $successColor}}
-						{{dbSet $user.ID "EconomyInfo" (sdict "cash" $newCashBalance "bank" $newBankBalance)}}
-					{{else}}
-						{{$embed.Set "description" (print "Invalid amount.\nCommand syntax is `" $.Cmd " <Amount:Amount>`")}}
+				{{$amount := (index $.CmdArgs 0)}}
+				{{if (toInt $amount)}}
+					{{if gt (toInt $amount) (toInt $bank)}}
+						{{$embed.Set "description" (print "You're unable to withdraw more than you have in your bank.\nYou currently have " $symbol (humanizeThousands $bank) " in your bank.")}}
 						{{$embed.Set "color" $errorColor}}
+					{{else}}
+						{{$embed.Set "description" (print "You withdrew " $symbol (humanizeThousands $amount) " from your bank!")}}
+						{{$embed.Set "color" $successColor}}
+						{{$cash = add $cash $amount}}
+						{{$bank = sub $bank $amount}}
 					{{end}}
+				{{else if eq (lower (toString $amount)) "all"}}
+					{{$embed.Set "description" (print "You withdrew " $symbol (humanizeThousands $bank) " from your bank!")}}
+					{{$embed.Set "color" $successColor}}
+					{{$cash = add $bank $cash}}
+					{{$bank = (toInt 0)}}
+				{{else}}
+					{{$embed.Set "description" (print "Invalid amount.\nCommand syntax is `" $.Cmd " <Amount:Amount>`")}}
+					{{$embed.Set "color" $errorColor}}
 				{{end}}
 			{{else}}
 				{{$embed.Set "description" (print "No `amount` argument provided.\nCommand syntax is `" $.Cmd " <Amount:Amount>`")}}
 				{{$embed.Set "color" $errorColor}}
 			{{end}}
 		{{end}}
+		{{$bankDB.Set $userID $bank}}
+		{{dbSet 0 "bank" $bankDB}}
+		{{dbSet $userID "cash" $cash}}
 	{{else}}
 		{{$embed.Set "description" (print "No `Settings` database found.\nPlease set it up with the default values using `" $prefix "set default`")}}
 		{{$embed.Set "color" $errorColor}}
