6a5615abbff137b030b32355c50ad8bb0c35a9cb
Ranger-4297 <a.rhykerw@gmail.com>
2023-04-14 13:49:48 +0100

    Feat(leaveMessage):Rework balance system and cleanup
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>

--- Economy/Settings/leaveMessageAlt.cc.go
+++ Economy/Settings/leaveMessageAlt.cc.go
@@ -17,15 +17,21 @@ To retrieve a users economy data upon rejoining
 {{/* Only edit below if you know what you're doing (: rawr */}}
 
 {{/* Response */}}
-{{with dbGet .User.ID "EconomyInfo"}}
-	{{$a := sdict .Value}}
-	{{if not (dbGet 0 "EconomyInfoLeftGuild")}}
-		{{dbSet 0 "EconomyInfoLeftGuild" sdict}}
+{{$cash := or (dbGet .User.ID "cash").Value 0 | toInt}}
+{{$bank := or (((dbGet 0 "bank").Value).Get (toString $user.ID)) 0 | toInt}}
+{{$economyData := or ($a := (dbGet $userID "userEconData").Value) ($a := sdict "inventory" sdict "streaks" (sdict "daily" 0 "weekly" 0 "monthly" 0))}}
+{{with or ($a := (dbGet 0 "EconomyInfoLeftGuild").Value) sdict}}
+	{{$a.Set (toString $.User.ID) (sdict "cash" $cash "bank" $bank "data" $economyData)}}
+	{{dbSet 0 "EconomyInfoLeftGuild" $a}}
+	{{if $cash}}
+		{{dbDel $.User.ID "cash"}}
 	{{end}}
-	{{with dbGet 0 "EconomyInfoLeftGuild"}}
-		{{$entry := sdict .Value}}
-		{{$entry.Set (toString $.User.ID) (sdict $a)}}
-		{{dbSet 0 "EconomyInfoLeftGuild" $entry}}
-		{{dbDel $.User.ID "EconomyInfo"}}
+	{{if $bank}}
+		{{$bankDB := (dbGet 0 "bank").Value}}
+		{{$bankDB.Del (toString $.User.ID)}}
+		{{dbSet 0 "bank" $bankDB}}
+	{{end}}
+	{{if $economyData}}
+		{{dbDel $.User.ID "userEconData"}}
 	{{end}}
 {{end}}
