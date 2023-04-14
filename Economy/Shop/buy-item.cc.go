65988fdd47a6f689e6377d16603a33dfda1e7d42
Ranger-4297 <a.rhykerw@gmail.com>
2023-04-14 19:06:48 +0100

    Feat(buyItem):Rework balance system and cleanup
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>

--- Economy/Shop/buy-item.cc.go
+++ Economy/Shop/buy-item.cc.go
@@ -27,83 +27,74 @@
 {{with dbGet 0 "EconomySettings"}}
 	{{$a := sdict .Value}}
 	{{$symbol := $a.symbol}}
-	{{with (dbGet $userID "EconomyInfo")}}
-		{{$a = sdict .Value}}
-		{{$bal := $a.cash}}
-		{{$inventory := sdict}}
-		{{with (dbGet 0 "store")}}
-			{{$info := sdict .Value}}
-			{{$items := sdict}}
-			{{if ($info.Get "Items")}}
-				{{$items = sdict ($info.Get "Items")}}
-				{{with $.CmdArgs}}
-					{{$name := (index . 0)}}
-					{{if $items.Get $name}}
-						{{$item := $items.Get (index . 0)}}
-						{{$price := $item.Get "price"}}
-						{{$qty := $item.Get "quantity"}}
-						{{$uqty := 0}} {{/* USER QUANTITY */}}
-						{{$inventory := $a.Get "inventory"}}
-						{{if ($inventory.Get $name)}} {{/* Checks if user has item with name*/}}
-							{{$invitem := ($inventory.Get $name)}}
-							{{$uqty = ($invitem.Get "qty")}}
-						{{end}}
-						{{$bqty := 1}}
-						{{if gt (len $.CmdArgs) 1}}
-							{{$inp := (index . 1)}}
-							{{if (toInt $inp)}}
-								{{if lt (toInt $bqty) (toInt $qty)}}
-									{{$bqty = (mult $bqty $inp)}}
-									{{$price = (mult $price $inp)}}
-								{{end}}
-							{{else}}
-								{{if (eq $inp "all" "max")}}
-									{{$bqty = (mult (toInt $bqty) (toInt $qty))}}
-									{{$price = (mult (toInt $price) (toInt $qty))}}
-								{{end}}
+	{{$bal := or (dbGet $userID "cash").Value 0 | toInt}}
+	{{$userdata := or (dbGet $userID "userEconData").Value (sdict "inventory" sdict "streaks" (sdict "daily" 0 "weekly" 0 "monthly" 0))}}
+	{{$inventory := $userdata.inventory}}
+	{{with (dbGet 0 "store")}}
+		{{$info := sdict .Value}}
+		{{$items := sdict}}
+		{{if ($info.Get "items")}}
+			{{$items = sdict ($info.Get "items")}}
+			{{with $.CmdArgs}}
+				{{$name := (index . 0)}}
+				{{if $items.Get $name}}
+					{{$item := $items.Get (index . 0)}}
+					{{$price := $item.Get "price"}}
+					{{$qty := $item.Get "quantity"}}
+					{{$uqty := 0}} {{/* USER QUANTITY */}}
+					{{if ($inventory.Get $name)}} {{/* Checks if user has item with name*/}}
+						{{$invitem := ($inventory.Get $name)}}
+						{{$uqty = ($invitem.Get "quantity")}}
+					{{end}}
+					{{$bqty := 1}}
+					{{if gt (len $.CmdArgs) 1}}
+						{{$inp := (index . 1)}}
+						{{if (toInt $inp)}}
+							{{if lt (toInt $bqty) (toInt $qty)}}
+								{{$bqty = (mult $bqty $inp)}}
+								{{$price = (mult $price $inp)}}
 							{{end}}
-						{{end}}
-						{{if le (toInt $price) (toInt $bal)}}
-							{{$nqty := (sub $qty $bqty)}}
-							{{if eq (toInt $nqty) 0}}
-								{{$items.Del $name}}
-								{{$info.Set "Items" $items}}
-								{{dbSet 0 "store" $info}}
-							{{else}}
-								{{$item.Set "quantity" $nqty}}
-								{{$items.Set $name $item}}
-								{{$info.Set "Items" $items}}
-								{{dbSet 0 "store" $info}}
-							{{end}}
-							{{$nbal := (sub $bal $price)}}
-							{{$inventory := (sdict $name (sdict "desc" ($item.Get "desc") "quantity" (add $uqty $bqty)))}}
-							{{$a.Set "cash" $nbal}}
-							{{$a.Set "inventory" $inventory}}
-							{{dbSet $userID "EconomyInfo" $a}}
-							{{$embed.Set "description" (print "Purchase successful!\nYou're now a proud owner of a(n) " $name)}}
-							{{$embed.Set "color" $successColor}}
 						{{else}}
-							{{$embed.Set "description" (print "You don't have enough money for this")}}
-							{{$embed.Set "color" $errorColor}}
+							{{if (eq $inp "all" "max")}}
+								{{$bqty = (mult (toInt $bqty) (toInt $qty))}}
+								{{$price = (mult (toInt $price) (toInt $qty))}}
+							{{end}}
 						{{end}}
+					{{end}}
+					{{if le (toInt $price) (toInt $bal)}}
+						{{$nqty := (sub $qty $bqty)}}
+						{{if eq (toInt $nqty) 0}}
+							{{$items.Del $name}}
+						{{else}}
+							{{$item.Set "quantity" $nqty}}
+							{{$items.Set $name $item}}
+						{{end}}
+						{{$info.Set "items" $items}}
+						{{dbSet 0 "store" $info}}
+						{{$bal = (sub $bal $price)}}
+						{{$inventory.Set $name (sdict "desc" ($item.Get "desc") "quantity" (add $uqty $bqty))}}
+						{{$userdata.Set "inventory" $inventory}}
+						{{$embed.Set "description" (print "Purchase successful!\nYou're now a proud owner of a(n) " $name)}}
+						{{$embed.Set "color" $successColor}}
 					{{else}}
-						{{$embed.Set "description" (print "Invalid item argument provided :(\nSyntax is `" $.Cmd " <Name> [Quantity:Int]`\n\nTo view all items, run the `" $prefix "shop` command.")}}
+						{{$embed.Set "description" (print "You don't have enough money for this")}}
 						{{$embed.Set "color" $errorColor}}
 					{{end}}
 				{{else}}
-					{{$embed.Set "description" (print "No item argument provided :(\nSyntax is `" $.Cmd " <Name> [Quantity:Int]`\n\nTo view all items, run the `" $prefix "shop` command.")}}
+					{{$embed.Set "description" (print "Invalid item argument provided :(\nSyntax is `" $.Cmd " <Name> [Quantity:Int]`\n\nTo view all items, run the `" $prefix "shop` command.")}}
 					{{$embed.Set "color" $errorColor}}
 				{{end}}
 			{{else}}
-				{{$embed.Set "description" (print "There are no items :(\nAdd some items with `" $prefix "create-item <Name> <Price:Int> <Quantity:Int> <Description:String>`")}}
+				{{$embed.Set "description" (print "No item argument provided :(\nSyntax is `" $.Cmd " <Name> [Quantity:Int]`\n\nTo view all items, run the `" $prefix "shop` command.")}}
 				{{$embed.Set "color" $errorColor}}
 			{{end}}
+		{{else}}
+			{{$embed.Set "description" (print "There are no items :(\nAdd some items with `" $prefix "create-item <Name> <Price:Int> <Quantity:Int> <Description:String>`")}}
+			{{$embed.Set "color" $errorColor}}
 		{{end}}
-	{{else}}
-		{{dbSet $userID "EconomyInfo" (sdict "cash" 200 "bank" 0)}}
-		{{$embed.Set "description" (print "You were not in the economy database....adding you now\nPlease try again")}}
-		{{$embed.Set "color" $errorColor}}
 	{{end}}
+	{{dbSet $userID "userEconData" $userdata}}
+	{{dbSet $userID "cash" $bal}}
 {{else}}
 	{{$embed.Set "description" (print "No `Settings` database found.\nPlease set it up with the default values using `" $prefix "set default`")}}
 	{{$embed.Set "color" $errorColor}}
