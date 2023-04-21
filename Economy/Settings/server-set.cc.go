682d5abd6f71383763dedc2eb80daab326f1de92
Ranger-4297 <a.rhykerw@gmail.com>
2023-04-21 23:34:54 +0100

    feat:Rename server settings in anticipation of user settings
    Signed-off-by: Ranger-4297 <a.rhykerw@gmail.com>

--- Economy/Settings/server-set.cc.go
+++ Economy/Settings/server-set.cc.go
@@ -2,7 +2,7 @@
 		Made by Ranger (765316548516380732)
 
 	Trigger Type: `Regex`
-	Trigger: `\A(-|<@!?204255221017214977>\s*)(set|config(ure)?)(\s+|\z)`
+	Trigger: `\A(-|<@!?204255221017214977>\s*)server-?(set|config(ure)?)(\s+|\z)`
 
 	©️ Ranger 2020-Present
 	GNU, GPLV3 License
