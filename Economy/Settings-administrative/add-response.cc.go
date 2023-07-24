59c0b472a399e11852e4da6f7069d2ce2bef50f5
Ranger-4297 <a.rhykerw@gmail.com>
2023-07-24 20:28:40 +0100

    Rehaul response command
    Wiki update required

--- Economy/Settings-administrative/add-response.cc.go
+++ Economy/Settings-administrative/add-response.cc.go
@@ -30,62 +30,71 @@
 		{{$responses := or ($db.responses) (sdict "crime" cslice "work" cslice)}}
 		{{with $.CmdArgs}}
 			{{$type := (index $.CmdArgs 0) | lower}}
-			{{if eq $type "work" "crime"}}
-				{{if gt (len $.CmdArgs) 1}}
-					{{$response := (joinStr " " (slice $.CmdArgs 1))}}
-					{{if (reFind `\(amount\)` $response)}}
-						{{$currentResponses := $responses.Get $type}}
-						{{$currentResponses = $currentResponses.Append $response}}
-						{{$responses.Set $type $currentResponses}}
-						{{$embed.Set "description" (print "Successfully added \"" (reReplace `\(amount\)` $response "`(amount)`") "\" to the list of responses.\nTo view the list of responses in it's entirey. Please use the command `" $.Cmd " list`")}}
-						{{$embed.Set "color" $successColor}}
-						{{$db.Set "responses" $responses}}
-					{{else}}
-						{{$embed.Set "description" (print "Please include the exact string `(amount)` as a placeholder for where the amount goes.")}}
-						{{$embed.Set "color" $errorColor}}
-					{{end}}
-				{{else}}
-					{{$embed.Set "description" (print "No `Response` argument passed.\nSyntax is: `" $.Cmd " " $type " <Reponse>`")}}
-					{{$embed.Set "color" $errorColor}}
-				{{end}}
-			{{else if eq $type "list"}}
-				{{if gt (len $.CmdArgs) 1}}
-					{{$type := (index $.CmdArgs 1) | lower}}
+			{{if eq $type "work" "crime" "list"}}
+				{{if reFind `add-?responses?` $.Cmd}}
 					{{if eq $type "work" "crime"}}
-						{{$typeResponses := $responses.Get $type}}
-						{{if $typeResponses}}
-							{{$responseNumber := 0}}
-							{{$fields := cslice}}
-							{{range $typeResponses}}
-								{{- $fields = $fields.Append (sdict "Name" (print "Response: " $responseNumber) "value" (reReplace `\(amount\)` . "`(amount)`") "inline" false) -}}
-								{{$responseNumber = $responseNumber | add 1}}
+						{{if gt (len $.CmdArgs) 1}}
+							{{$response := (joinStr " " (slice $.CmdArgs 1))}}
+							{{if (reFind `\(amount\)` $response)}}
+								{{$currentResponses := $responses.Get $type}}
+								{{$currentResponses = $currentResponses.Append $response}}
+								{{$responses.Set $type $currentResponses}}
+								{{$embed.Set "description" (print "Successfully added \"" (reReplace `\(amount\)` $response "`(amount)`") "\" to the list of responses for `" $type "`.\nTo view the list of responses in it's entirey. Please use the command `" $prefix "responses list`")}}
+								{{$embed.Set "color" $successColor}}
+								{{$db.Set "responses" $responses}}
+							{{else}}
+								{{$embed.Set "description" (print "Please include the exact string `(amount)` as a placeholder for where the amount goes.")}}
+								{{$embed.Set "color" $errorColor}}
 							{{end}}
-							{{$embed.Set "description" (print "All responses for " $type "-based payouts are\nPlease note. This doesn't support pages yet.")}}
-							{{$embed.Set "fields" $fields}}
-							{{$embed.Set "color" $successColor}}
 						{{else}}
-							{{$embed.Set "description" (print "No responses for this type have been set.\nAdd some with `" $.Cmd " <Type:Work/Crime> <Response>`")}}
+							{{$embed.Set "description" (print "No `Response` argument passed.\nSyntax is: `" $.Cmd " " $type " <Reponse>`")}}
+							{{$embed.Set "color" $errorColor}}
+						{{end}}
+					{{end}}
+				{{else if reFind `responses?` $.Cmd}}
+					{{if gt (len $.CmdArgs) 1}}
+						{{$type := (index $.CmdArgs 1) | lower}}
+						{{if eq $type "work" "crime"}}
+							{{$typeResponses := $responses.Get $type}}
+							{{if $typeResponses}}
+								{{$responseNumber := 1}}
+								{{$fields := cslice}}
+								{{range $typeResponses}}
+									{{- $fields = $fields.Append (sdict "Name" (print "Response: " $responseNumber) "value" (reReplace `\(amount\)` . "`(amount)`") "inline" false) -}}
+									{{$responseNumber = $responseNumber | add 1}}
+								{{end}}
+								{{$embed.Set "description" (print "All responses for " $type "-based payouts are\nPlease note. This doesn't support pages yet.")}}
+								{{$embed.Set "fields" $fields}}
+								{{$embed.Set "color" $successColor}}
+							{{else}}
+								{{$embed.Set "description" (print "No responses for this type have been set.\nAdd some with `" $prefix "add-response <Type:Work/Crime> <Response>`")}}
+								{{$embed.Set "color" $errorColor}}
+							{{end}}
+						{{else}}
+							{{$embed.Set "description" (print "Invalid `type` argument passed.\nSyntax is: `" $.Cmd " list <Type:Work/Crime>`")}}
 							{{$embed.Set "color" $errorColor}}
 						{{end}}
 					{{else}}
-						{{$embed.Set "description" (print "Invalid `type` argument passed.\nSyntax is: `" $.Cmd " list <Type:Work/Crime>`")}}
+						{{$embed.Set "description" (print "No `type` argument passed.\nSyntax is: `" $.Cmd " list <Type:Work/Crime>`")}}
 						{{$embed.Set "color" $errorColor}}
 					{{end}}
-				{{else}}
-					{{$embed.Set "description" (print "No `type` argument passed.\nSyntax is: `" $.Cmd " list <Type:Work/Crime>`")}}
-					{{$embed.Set "color" $errorColor}}
 				{{end}}
 			{{else}}
-				{{$embed.Set "description" (print "Invalid `type` argument passed.\nSyntax is: `" $.Cmd " <Type:Work/Crime> <Reponse>`")}}
+				{{if reFind `add-?responses?` $.Cmd}}
+					{{$embed.Set "description" (print "Invalid `type` argument passed.\nSyntax is: `" $.Cmd " <Type:Work/Crime> <Reponse>`")}}
+				{{else if reFind `responses?` $.Cmd}}
+					{{$embed.Set "description" (print "Invalid `CommandType` argument passed.\nSyntax is: `" $.Cmd " <CommandType:List> <Type:Work/Crime>`")}}
+				{{end}}
 				{{$embed.Set "color" $errorColor}}
 			{{end}}
 		{{else}}
-			{{$embed.Set "description" (print "No `type` argument passed.\nSyntax is: `" $.Cmd " <Type:Work/Crime> <Reponse>`")}}
+			{{if reFind `add-?responses?` $.Cmd}}
+				{{$embed.Set "description" (print "No `type` argument passed.\nSyntax is: `" $.Cmd " <Type:Work/Crime> <Reponse>`")}}
+			{{else if reFind `responses?` $.Cmd}}
+				{{$embed.Set "description" (print "No `CommandType` argument passed.\nSyntax is: `" $.Cmd " <CommandType:List> <Type:Work/Crime>`")}}
+			{{end}}
 			{{$embed.Set "color" $errorColor}}
 		{{end}}
-	{{else}}
-		{{$embed.Set "description" (print "No `type` argument passed.\nSyntax is: `" $.Cmd " <Type:Work/Crime> <Reponse>`")}}
-		{{$embed.Set "color" $errorColor}}
 	{{end}}
 {{else}}
 	{{$embed.Set "description" (print "Insufficient permissions.\nTo use this command you need to have either `Administrator` or `ManageServer` permissions")}}
