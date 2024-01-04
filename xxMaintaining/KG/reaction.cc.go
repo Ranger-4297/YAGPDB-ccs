67ba5066276a726520306b3508b758619847ca3a
Ranger-4297 <a.rhykerw@gmail.com>
2024-01-04 19:13:49 +0000

    feat: Add custom reaction role and message for #rankChannel

--- xxMaintaining/KG/reaction.cc.go
+++ xxMaintaining/KG/reaction.cc.go
@@ -10,7 +10,7 @@
 
 
 {{/* Configuration values start */}}
-{{$verifyingRole := 1191469975416864818}}		{{/* roleID of SERVERJOIN role */}}
+{{$verifyingRole := 1191469975416864818}}		{{/* roleID of VERIFYING role */}}
 {{$enrollingRole := 1190603830065373224}}		{{/* roleID of ENROLLING role */}}
 {{$onboardingRole := 1189413709454520360}}		{{/* roleID of onboarding role */}}
 {{$serverRole := 1185703179254505582}}			{{/* roleID of SERVER role */}}
@@ -24,6 +24,11 @@
 {{$rankChannel := 1185723603086491648}}			{{/* channelID of RANK channel */}}
 {{$rulesChannel := 1185730875644706826}}        {{/* channelID of RULES channel */}}
 {{$publicChat := 1191231631726493706}}			{{/* channelID of PUBLIC CHAT channel*/}}
+{{$r1Role := 1185724333654540339}}				{{/* roleID of R1 role */}}
+{{$r2Role := 1185724406002094190}}				{{/* roleID of R2 role */}}
+{{$r3Role := 1185724444698751067}}				{{/* roleID of R3 role */}}
+{{$r4Role := 1185724479138173049}}				{{/* roleID of R4 role */}}
+{{$r5Role := 1185724508322148363}}				{{/* roleID of R5 role */}}
 {{/* Configuration values end */}}
 
 {{/* response */}}
@@ -53,6 +58,27 @@
 	{{$m2 := sendMessageNoEscapeRetID $serverChannel (print (reReplace `<@!>` ($language.Get 16) .User.Mention))}}
 	{{deleteMessage $serverChannel $m2 60}}
 {{else if eq .Channel.ID $rankChannel}}
+	{{$rankedRole := ""}}
+	{{with .Reaction.Emoji.Name}}
+		{{if eq . "1️⃣"}}
+			{{$rankedRole = $r1Role}}
+		{{else if eq . "2️⃣"}}
+			{{$rankedRole = $r2Role}}
+		{{else if eq . "3️⃣"}}
+			{{$rankedRole = $r3Role}}
+		{{else if eq . "4️⃣"}}
+			{{$rankedRole = $r4Role}}
+		{{else if eq . "5️⃣"}}
+			{{$rankedRole = $r5Role}}
+		{{end}}
+	{{end}}
+	{{$rankRoles := cslice $r1Role $r2Role $r3Role $r4Role $r5Role}}
+	{{range $rankRoles}}
+		{{if in $.Member.Roles .}}
+			{{removeRoleID .}}
+		{{end}}
+	{{end}}
+	{{addRoleID $rankedRole}}
 	{{addRoleID $rankRole}}
 	{{if $cd := dbGet .User.ID "Rrcooldown3"}}
 		{{return}}
@@ -60,8 +86,8 @@
 	{{dbSetExpire .User.ID "Rrcooldown3" true 3}}
 	{{$m := sendMessageNoEscapeRetID $rankChannel (print (reReplace `<#>` (reReplace `<@!>` ($language.Get 19) .User.Mention) (printf "<#%d>" $rulesChannel)))}}
 	{{deleteMessage $rankChannel $m 60}}
-	{{$m2 := sendMessageNoEscapeRetID $rulesChannel (print (reReplace `<@!>` ($language.Get 17) .User.Mention))}}
-	{{deleteMessage $rulesChannel $m2 60}}
+	{{$m2 := sendMessageNoEscapeRetID $rulesChannel (print (reReplace `<@!>` ($language.Get 21) .User.Mention))}}
+	{{addMessageReactions $rulesChannel $m2 "✅"}}
 {{else if eq .Channel.ID $rulesChannel}}
 	{{addRoleID $verifyRole}}
 	{{range (cslice $verifyingRole $serverRole $allianceRole $nameRole $enrollingRole $onboardingRole $rankRole)}}
@@ -73,6 +99,5 @@
 		{{return}}
 	{{end}}
 	{{dbSetExpire .User.ID "Rrcooldown5" true 60}}
-	{{$m := sendMessageNoEscapeRetID $publicChat (print (reReplace `<@!>` ($language.Get 18) .User.Mention))}}
-	{{deleteMessage $publicChat $m 60}}
+	{{sendMessageNoEscape $publicChat (print (reReplace `<@!>` ($language.Get 18) .User.Mention))}}
 {{end}}
